531c93ad448abd3546388f3f05752eeb7c466b99
AbhishekAmeta7 <abhishekameta55@gmail.com>
2024-12-07 20:54:33 +0530

    String Concat Without Using + Operator

---
+++ StringPractice1/StringPractice39.java
@@ -0,0 +1,8 @@
+public class StringPractice39 {
+    public static void main(String[] args) {
+        String s = "Hello";
+        s = s.concat(" World");
+        System.out.println(s); 
+    }
+}
+// Write a Java program to concatenate two strings without using the + operator.
