257f01e468f2ca5cd1bf72c1a8342124e6b53085
AbhishekAmeta7 <abhishekameta55@gmail.com>
2024-12-09 12:28:25 +0530

    Remove WhiteSpace from a string

---
+++ StringPractice1/StringPractice49.java
@@ -0,0 +1,8 @@
+public class StringPractice49 {
+    public static void main(String[] args) {
+        String s = "Java is Platform Independent Language";
+        s = s.replace(" ", "");
+        System.out.println(s);
+    }
+}
+// Write a Java program to remove all spaces from a string.
