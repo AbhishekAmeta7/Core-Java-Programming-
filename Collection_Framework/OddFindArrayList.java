e1a44627c6be5fd9136938b0f8f38f9cc3db23be
AbhishekAmeta7 <abhishekameta55@gmail.com>
2024-12-10 21:54:20 +0530

    Find Odd Element From An Array

---
+++ Collection_Framework/OddFindArrayList.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class OddFindArrayList {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> al = new ArrayList<>();
+        System.out.print("Enter Size Of ArrayList: ");
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            al.add(x);
+        }
+        for (int i = 0; i < al.size(); i++) {
+            if(al.get(i)%2 !=0){
+                System.out.print(al.get(i)+" ");
+            }
+        }
+    }
+}
+// odd number find in arraylist 
